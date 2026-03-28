a464163c9e09b64288a03bf582780dabc3e3a3ed
Ivan Krotov <ivankrotov910@gmail.com>
2026-03-28 12:27:02 +0300

    Инициализация проекта в ветке main. Базовые настройки InventoryForm

--- InventoryManagement/InventoryForm.Designer.cs
+++ InventoryManagement/InventoryForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace InventoryManagement
 {
-    partial class Form1
+    partial class InventoryForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,10 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InventoryForm));
+            this.SuspendLayout();
+            // 
+            // InventoryForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "InventoryForm";
+            this.Text = "Управление инвентарем";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- InventoryManagement/InventoryForm.cs
+++ InventoryManagement/InventoryForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace InventoryManagement
 {
-    public partial class Form1 : Form
+    public partial class InventoryForm : Form
     {
-        public Form1()
+        public InventoryForm()
         {
             InitializeComponent();
         }
